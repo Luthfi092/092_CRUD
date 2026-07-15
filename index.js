a70e4af63e554816e112acd347b1d133fc47692d
upi1682 <upi538287@gmail.com>
2026-07-15 14:29:07 +0700

    membuat dan menambahkan middleware

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+
+
+
