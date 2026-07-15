3ca6fe6362903227285aff3698c5e44b180ab27a
upi1682 <upi538287@gmail.com>
2026-07-15 14:41:10 +0700

    Inisialisasi Express dan koneksi PostgreSQL

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Koneksi PostgreSQL
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'mahasiswaku',
+    password: '5k3batang',
+    port: 5432,
+});
+
+pool.connect()
+    .then(() => console.log('Koneksi ke PostgreSQL berhasil!'))
+    .catch(err => console.error('Koneksi gagal:', err));
+
 
 
 
